6d0be644db9327f81a1c019fd8fb2910250c921a
macc <maciej.rajtar@gmail.com>
2024-10-19 11:00:36 +0200

    Senko version with public view

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
+import time
+from machine import Pin
+
+status_led = Pin(2, Pin.OUT)
 
 
-print('Test no.2')
-print('Disconnected')
+def main():
+    second = 0
+    minute = 0
+    tick = 0
+    led_state = status_led.value()  # Początkowy stan LED
+    led_on_time = 10  # Czas włączenia LED w tickach (1 tick = 0.1 s)
+    led_off_time = 10  # Czas wyłączenia LED w tickach (1 tick = 0.1 s)
+    led_timer = 0  # Licznik do kontroli czasu migania LED
+
+    while True:
+        # Zarządzanie czasem (licznik sekund, minut)
+        if tick == 10:  # 1 sekunda (tick = 0.1 sekundy)
+            tick = 0
+            second += 1
+            if second == 59:
+                second = 0
+                minute += 1
+            if minute == 15:
+                machine.reset()  # Reset po 15 minutach
+        time.sleep(0.1)  # Odstęp czasu dla ticka
+        tick += 1
+
+        # Miganie LED bez blokowania pętli
+        led_timer += 1
+        if led_state and led_timer >= led_on_time:  # LED włączony przez zadany czas
+            status_led.off()
+            led_state = False
+            led_timer = 0  # Reset licznika
+        elif not led_state and led_timer >= led_off_time:  # LED wyłączony przez zadany czas
+            status_led.on()
+            led_state = True
+            led_timer = 0  # Reset licznika
+
+
+main()
